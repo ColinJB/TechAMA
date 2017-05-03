d32fc1bd7b0f4438dd6b5fb1eb19e75dfd92cd03
Colin Bloom <colinjbloom113@gmail.com>
2017-05-03 02:52:00 -0600

    add save/postquestion

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,5 +6,18 @@ export default Ember.Route.extend({
       questions: this.store.findAll('question'),
       answers: this.store.findAll('answer')
     });
+  },
+
+  actions: {
+    destroyQuestion(question) {
+      question.destroyRecord();
+      this.transitionTo('index');
+    },
+
+    saveQuestion(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    },
   }
 });
